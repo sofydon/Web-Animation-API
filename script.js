174bed06a7c19b4856ba5560010df3b59ceb5aaf
sofydon <petinova@bk.ru>
2022-05-08 16:02:53 -0400

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const btnPhone = document.querySelector('#iphone'),
+      btnMacbook = document.querySelector('#macbook'),
+      images = document.querySelectorAll('img');
+
+let phoneAnimation;
+btnPhone.addEventListener('click', () => {
+    if (!phoneAnimation) {
+        phoneAnimation = images[0].animate([
+            {transform: 'translateY(0)'},
+            {transform: 'translateY(100px)'},
+            {transform: 'translateY(-100px)'},
+            {transform: 'translateY(0)'}
+        ], {
+            duration: 3000, 
+            iterations: Infinity
+        });
+    } else if (phoneAnimation.playState === 'paused') {
+        phoneAnimation.play();
+    } else {
+        phoneAnimation.pause();
+    }
+});
+
